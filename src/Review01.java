baabdb5ca3b8c47766b65bfc25269eb4a1460b08
kenryo1025 <sakata.kenryo@linecorp.com>
2024-09-20 15:55:49 +0900

    Lesson3課題の作成

---
+++ src/Review01.java
@@ -0,0 +1,16 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+
+        int syo=1500;
+        int syo_tax=tax(1500);
+        System.out.println(syo +"円の商品の税込み価格は" +(syo+syo_tax)+"円(消費税は"+ syo_tax+"円)です。");
+    }
+
+    public static int tax(int syo) {
+        int tax_par=10;
+        int syo_tax = syo / tax_par;
+        return syo_tax;
+    }
+}
